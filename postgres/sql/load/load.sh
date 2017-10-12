e52588009c416ebb39330e3bdc65d9ad2b7827d1
Gabor Szarnyas <szarnyasg@gmail.com>
2017-10-12 10:28:47 -0400

    Rename SQL script to avoid same name as the Shell script

--- postgres/sql/load/load.sh
+++ postgres/sql/load/load.sh
@@ -7,5 +7,5 @@ PG_USER=${3:-$USER}
 /usr/bin/dropdb $DBNAME -U $PG_USER
 /usr/bin/createdb $DBNAME -U $PG_USER --template template0 -l "C"
 /usr/bin/psql -d $DBNAME -U $PG_USER -a -f schema.sql
-(cat load.sql | sed "s|PATHVAR|$DB_DIR|g"; echo "\q\n") | /usr/bin/psql -d $DBNAME -U $PG_USER
+(cat snb-load.sql | sed "s|PATHVAR|$DB_DIR|g"; echo "\q\n") | /usr/bin/psql -d $DBNAME -U $PG_USER
 /usr/bin/psql -d $DBNAME -U $PG_USER -a -f schema_constraints.sql
