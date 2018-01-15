557ea42b848d20363d941e6c501e6e2f0464b708
Jozsef Marton <marton@db.bme.hu>
2018-01-15 21:29:28 +0100

    Fix ORDER BY in bi-12.sql

--- postgres/queries/bi/query12.sql
+++ postgres/queries/bi/query12.sql
@@ -1,16 +1,20 @@
+/* Q12. Trending Posts
+\set date '\'2011-07-22T00:00:00.000+00:00\''::timestamp
+\set likeThreshold 400
+ */
 select
   ps_postid,
   p_firstname,
   p_lastname,
   ps_creationdate,
-  count(*)
+  count(*) as likeCount
 from post, person, likes
-where ps_postid in (select l_postid from likes group by l_postid having count(*) > $likeThreshold)
+where ps_postid in (select l_postid from likes group by l_postid having count(*) > :likeThreshold)
   and p_personid = ps_creatorid
   and l_postid = ps_postid
-  and ps_creationdate > $date
+  and ps_creationdate > :date
 group by ps_postid, p_firstname, p_lastname, ps_creationdate
 order by
-  ps_creationdate desc,
+  likeCount desc,
   ps_postid asc
 limit 100;
