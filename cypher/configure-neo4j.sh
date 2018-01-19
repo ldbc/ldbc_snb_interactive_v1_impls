ec7c7b77a0fa12f03146f757d8ba1809b9b84392
Gabor Szarnyas <szarnyasg@gmail.com>
2018-01-19 14:31:02 +0100

    Export environment vars properly for Neo4j

--- cypher/configure-neo4j.sh
+++ cypher/configure-neo4j.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-export NEO4J_HOME=`pwd`/neo4j-server
-export DB_DIR=$NEO4_HOME/data/databases/graph.db
 $NEO4J_HOME/bin/neo4j-admin set-initial-password admin
---
+++ cypher/environment-variables-neo4j.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export NEO4J_HOME=`pwd`/neo4j-server
+export DB_DIR=$NEO4_HOME/data/databases/graph.db
