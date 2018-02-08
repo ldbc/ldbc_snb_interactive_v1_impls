310c3444e9380043c431af47aab1155030342865
Gabor Szarnyas <szarnyasg@gmail.com>
2018-02-08 18:12:51 +0100

    Add switch

--- sparql/load-to-stardog.sh
+++ sparql/load-to-stardog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 $STARDOG_HOME/bin/stardog-admin server start
-$STARDOG_HOME/bin/stardog-admin db create $RDF_DB
+$STARDOG_HOME/bin/stardog-admin db create -n $RDF_DB
 $STARDOG_HOME/bin/stardog add $RDF_DB $RDF_DATA_DIR/*.ttl
