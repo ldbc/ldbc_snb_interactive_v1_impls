7aa6b29d31c1eaf94d1154390992ed3127c2374e
Gabor Szarnyas <szarnyasg@gmail.com>
2018-02-08 18:14:10 +0100

    Fix command

--- sparql/load-to-stardog.sh
+++ sparql/load-to-stardog.sh
@@ -2,4 +2,4 @@
 
 $STARDOG_HOME/bin/stardog-admin server start
 $STARDOG_HOME/bin/stardog-admin db create -n $RDF_DB
-$STARDOG_HOME/bin/stardog add $RDF_DB $RDF_DATA_DIR/*.ttl
+$STARDOG_HOME/bin/stardog data add $RDF_DB $RDF_DATA_DIR/*.ttl
