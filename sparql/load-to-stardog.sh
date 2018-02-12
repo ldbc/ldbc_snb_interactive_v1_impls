1dd9df826885c27460adecb425920ea92f7f78dd
Gabor Szarnyas <szarnyasg@gmail.com>
2018-02-12 10:53:04 +0100

    SPARQL implementation: drop DB before loading

--- sparql/load-to-stardog.sh
+++ sparql/load-to-stardog.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 $STARDOG_HOME/bin/stardog-admin server start
+$STARDOG_HOME/bin/stardog-admin db drop $RDF_DB
 $STARDOG_HOME/bin/stardog-admin db create -n $RDF_DB
 $STARDOG_HOME/bin/stardog data add $RDF_DB $RDF_DATA_DIR/*.ttl
