d2da689b335fa934aec0d73f95091c4e14a9d5ec
Gabor Szarnyas <szarnyasg@gmail.com>
2019-09-26 17:29:02 +0200

    Remove Virtuoso/SPARQL implementation

--- sparql/interactive-validate.sh
+++ sparql/interactive-validate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp target/sparql-0.4.0-SNAPSHOT.jar com.ldbc.driver.Client -P interactive-validate-virtuoso.properties
+java -cp target/sparql-0.4.0-SNAPSHOT.jar com.ldbc.driver.Client -P interactive-validate.properties
--- sparql/src/test/java/sparql/SparqlInteractiveTest.java
+++ sparql/src/test/java/sparql/SparqlInteractiveTest.java
@@ -1,35 +1,21 @@
 package sparql;
 
-import com.google.common.collect.ImmutableList;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.*;
 import com.ldbc.impls.workloads.ldbc.snb.db.BaseDb;
 import com.ldbc.impls.workloads.ldbc.snb.interactive.InteractiveTest;
 import com.ldbc.impls.workloads.ldbc.snb.sparql.interactive.StardogInteractiveDb;
-import com.ldbc.impls.workloads.ldbc.snb.sparql.interactive.VirtuosoInteractiveDb;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SparqlInteractiveTest extends InteractiveTest {
 
-    private static final boolean isVirtuoso = false;
-
-    private static BaseDb getBaseDb(){
-        if (isVirtuoso) {
-            return new VirtuosoInteractiveDb();
-        }{
-            return new StardogInteractiveDb();
-        }
+    private static BaseDb getBaseDb() {
+        return new StardogInteractiveDb();
     }
 
-    //     Stardog
-    private static String endpointStardog = "http://localhost:5820/";
-    //     Virtuoso
-    private static String endpointVirtuoso = "localhost:1127";
+    private static String endpoint = "http://localhost:5820/";
     private static String databaseName = "ldbcsf1";
     private static String queryDir = "queries";
     private static String graphUri = "http://www.ldbc.eu";
@@ -38,18 +24,10 @@ public class SparqlInteractiveTest extends InteractiveTest {
         super(getBaseDb());
     }
 
-    public void setUp() {
-
-    }
-
     @Override
     public Map<String, String> getProperties() {
         final Map<String, String> properties = new HashMap<>();
-        if (isVirtuoso) {
-            properties.put("endpoint", endpointVirtuoso);
-        }{
-            properties.put("endpoint", endpointStardog);
-        }
+        properties.put("endpoint", endpoint);
         properties.put("graphUri", graphUri);
         properties.put("databaseName", databaseName);
         properties.put("queryDir", queryDir);
