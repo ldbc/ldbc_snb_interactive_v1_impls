bb5b6a537e794f51379715b9c17aed78c57cf813
Gabor Szarnyas <szarnyasg@gmail.com>
2020-03-06 23:32:58 +0100

    Minor improvements in Neo4j scripts

--- cypher/load-scripts/delete-neo4j-database.sh
+++ cypher/load-scripts/delete-neo4j-database.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 $NEO4J_HOME/bin/neo4j stop
+sleep 1
 rm -rf $NEO4J_DATA_DIR
--- cypher/load-scripts/restart-neo4j.sh
+++ cypher/load-scripts/restart-neo4j.sh
@@ -3,4 +3,4 @@
 $NEO4J_HOME/bin/neo4j restart
 
 echo Neo4j log:
-tail -n 20 $NEO4J_HOME/logs/neo4j.log
+tail -n 12 $NEO4J_HOME/logs/neo4j.log
