da3f62fd28c74729f2e048bce8fd7d6c52408925
Gabor Szarnyas <szarnyasg@gmail.com>
2020-04-26 19:37:01 +0200

    Make SparqlConverter thread-safe

--- sparql/src/main/java/com/ldbc/impls/workloads/ldbc/snb/sparql/converter/SparqlConverter.java
+++ sparql/src/main/java/com/ldbc/impls/workloads/ldbc/snb/sparql/converter/SparqlConverter.java
@@ -2,11 +2,11 @@ package com.ldbc.impls.workloads.ldbc.snb.sparql.converter;
 
 import com.ldbc.impls.workloads.ldbc.snb.converter.Converter;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.stream.Collectors;
 
 public class SparqlConverter extends Converter {
@@ -16,31 +16,23 @@ public class SparqlConverter extends Converter {
     public static final String SPARQL_DATETIME_RETURN_SHORT_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
     public static final String SPARQL_DATE_FORMAT = "yyyy-MM-dd";
 
+    ZoneId gmt = ZoneId.of("GMT");
 
-    final SimpleDateFormat sdfQuery = new SimpleDateFormat(SPARQL_DATETIME_QUERY_FORMAT);
-    final SimpleDateFormat sdfReturnLong = new SimpleDateFormat(SPARQL_DATETIME_RETURN_LONG_FORMAT);
-    final SimpleDateFormat sdfReturnShort = new SimpleDateFormat(SPARQL_DATETIME_RETURN_SHORT_FORMAT);
-    final SimpleDateFormat sdfDate = new SimpleDateFormat(SPARQL_DATE_FORMAT);
-
-    public SparqlConverter() {
-        sdfQuery.setTimeZone(TimeZone.getTimeZone("GMT"));
-        sdfReturnLong.setTimeZone(TimeZone.getTimeZone("GMT"));
-        sdfReturnShort.setTimeZone(TimeZone.getTimeZone("GMT"));
-        sdfDate.setTimeZone(TimeZone.getTimeZone("GMT"));
-    }
+    final DateTimeFormatter sdfQuery = DateTimeFormatter.ofPattern(SPARQL_DATETIME_QUERY_FORMAT).withZone(gmt);
+    final DateTimeFormatter sdfDate = DateTimeFormatter.ofPattern(SPARQL_DATE_FORMAT).withZone(gmt);
 
     @Override
     public String convertDateTime(Date date) {
-        return "\"" + sdfQuery.format(date) + "\"^^xsd:dateTime";
+        return "\"" + sdfQuery.format(date.toInstant()) + "\"^^xsd:dateTime";
     }
 
     @Override
     public String convertDate(long timestamp) {
-        return "\"" + sdfDate.format(new Date(timestamp)) + "\"^^xsd:date";
+        return "\"" + sdfDate.format(Instant.ofEpochMilli(timestamp)) + "\"^^xsd:date";
     }
 
-    public long convertDateToEpoch(String timestamp) throws ParseException {
-        return sdfDate.parse(timestamp).toInstant().toEpochMilli();
+    public long convertDateToEpoch(String timestamp) {
+        return Instant.from(sdfDate.parse(timestamp)).toEpochMilli();
     }
 
     public String convertString(String value) {
