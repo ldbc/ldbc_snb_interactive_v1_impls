4f5289d24e327a3376249f60bcd9221ed01b0da4
Gabor Szarnyas <szarnyasg@gmail.com>
2020-07-08 13:33:35 +0200

    Create indexes for Neo4j after loading

--- cypher/load-scripts/load-in-one-step.sh
+++ cypher/load-scripts/load-in-one-step.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-./delete-neo4j-database.sh && ./convert-csvs.sh && ./import-to-neo4j.sh && ./restart-neo4j.sh
+./delete-neo4j-database.sh && \
+	./convert-csvs.sh && \
+	./import-to-neo4j.sh && \
+	./restart-neo4j.sh && \
+	echo "Sleep for 10 seconds to let the database start." \
+	sleep 10 && \
+	./create-indices.sh && \
+	echo "Initiated asynchronous index creation. Please wait for a few minutes to let the database build them."
