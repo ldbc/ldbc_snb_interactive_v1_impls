4ee3bd78a3a19e75ac1f6de4726edcb241b81b0e
Gabor Szarnyas <szarnyasg@gmail.com>
2020-09-18 13:30:17 +0200

    Check that environment variables are set before loading

--- cypher/scripts/load-in-one-step.sh
+++ cypher/scripts/load-in-one-step.sh
@@ -9,6 +9,11 @@ echo NEO4J_DATA_DIR: $NEO4J_DATA_DIR
 echo NEO4J_CSV_POSTFIX: $NEO4J_CSV_POSTFIX
 echo ===============================================================================
 
+: ${NEO4J_HOME:?"Environment variable NEO4J_HOME is unset or empty"}
+: ${NEO4J_CSV_DIR:?"Environment variable NEO4J_CSV_DIR is unset or empty"}
+: ${NEO4J_DATA_DIR:?"Environment variable NEO4J_DATA_DIR is unset or empty"}
+: ${NEO4J_CSV_POSTFIX:?"Environment variable NEO4J_CSV_POSTFIX is unset or empty"}
+
 ./stop-neo4j-database.sh && \
   ./delete-neo4j-database.sh && \
   ./convert-csvs.sh && \
