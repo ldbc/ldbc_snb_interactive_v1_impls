137ac7d7d3227ffacb915ce4aad2b0de7f44b5e6
Gabor Szarnyas <szarnyasg@gmail.com>
2020-10-16 09:47:11 +0200

    Put new BI Q16-20 queries on ignore until we have SQL implementations

--- common/src/main/java/com/ldbc/impls/workloads/ldbc/snb/bi/BiTest.java
+++ common/src/main/java/com/ldbc/impls/workloads/ldbc/snb/bi/BiTest.java
@@ -5,6 +5,7 @@ import com.ldbc.driver.workloads.ldbc.snb.bi.*;
 import com.ldbc.driver.workloads.ldbc.snb.bi.LdbcSnbBiWorkload;
 import com.ldbc.impls.workloads.ldbc.snb.SnbTest;
 import com.ldbc.impls.workloads.ldbc.snb.db.BaseDb;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -90,26 +91,31 @@ public abstract class BiTest extends SnbTest {
         run(db, new LdbcSnbBiQuery15WeightedPaths(2199023264119L, 8796093028894L, 1275364800000L, 1277956800000L));
     }
 
+    @Ignore
     @Test
     public void testBiQuery16() throws DbException {
         run(db, new LdbcSnbBiQuery16FakeNewsDetection("Imelda_Marcos", 1317859200L, "Che", 1318377600L, 5, 10));
     }
 
+    @Ignore
     @Test
     public void testBiQuery17() throws DbException {
         run(db, new LdbcSnbBiQuery17InformationPropagationAnalysis("Elizabeth_Taylor", 10, 20));
     }
 
+    @Ignore
     @Test
     public void testBiQuery18() throws DbException {
         run(db, new LdbcSnbBiQuery18FriendRecommendation(290L, "Elizabeth_Taylor", 20));
     }
 
+    @Ignore
     @Test
     public void testBiQuery19() throws DbException {
         run(db, new LdbcSnbBiQuery19InteractionPathBetweenCities(1178L, 1142L));
     }
 
+    @Ignore
     @Test
     public void testBiQuery20() throws DbException {
         run(db, new LdbcSnbBiQuery20Recruitment("TajAir", 13194139533688L));
