8c42e443a530a723e99df4c538075354b9ca0c5d
Gabor Szarnyas <szarnyasg@gmail.com>
2020-11-24 16:36:50 +0100

    Omit changing labels to uppercase
    The generator now produces them as such and there is no need to sed the
    files

--- cypher/scripts/convert-csvs.sh
+++ cypher/scripts/convert-csvs.sh
@@ -13,14 +13,4 @@ while read line; do
   echo ${HEADER} | cat - <(tail -n +2 ${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX}) > tmpfile.csv && mv tmpfile.csv ${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX}
 done < headers.txt
 
-# replace labels with one starting with an uppercase letter
-sed -i.bkp "s/|city$/|City/" "${NEO4J_CSV_DIR}/static/place${NEO4J_CSV_POSTFIX}"
-sed -i.bkp "s/|country$/|Country/" "${NEO4J_CSV_DIR}/static/place${NEO4J_CSV_POSTFIX}"
-sed -i.bkp "s/|continent$/|Continent/" "${NEO4J_CSV_DIR}/static/place${NEO4J_CSV_POSTFIX}"
-sed -i.bkp "s/|company|/|Company|/" "${NEO4J_CSV_DIR}/static/organisation${NEO4J_CSV_POSTFIX}"
-sed -i.bkp "s/|university|/|University|/" "${NEO4J_CSV_DIR}/static/organisation${NEO4J_CSV_POSTFIX}"
-
-# remove .bkp files
-rm ${NEO4J_CSV_DIR}/*/*.bkp
-
 echo "Finished preprocessing CSV files"
