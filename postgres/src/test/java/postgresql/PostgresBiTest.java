fb3b9c8646edf761af61dbbf1d8743619ab1b7bb
Gabor Szarnyas <szarnyasg@gmail.com>
2020-12-15 22:31:56 +0100

    Ignore Postgres tests

--- postgres/src/test/java/postgresql/PostgresBiTest.java
+++ postgres/src/test/java/postgresql/PostgresBiTest.java
@@ -2,9 +2,11 @@ package postgresql;
 
 import com.ldbc.impls.workloads.ldbc.snb.bi.BiTest;
 import com.ldbc.impls.workloads.ldbc.snb.postgres.bi.PostgresBiDb;
+import org.junit.Ignore;
 
 import java.util.Map;
 
+@Ignore
 public class PostgresBiTest extends BiTest implements PostgresSnbTest {
 
     public PostgresBiTest() {
--- postgres/src/test/java/postgresql/PostgresInteractiveTest.java
+++ postgres/src/test/java/postgresql/PostgresInteractiveTest.java
@@ -2,9 +2,11 @@ package postgresql;
 
 import com.ldbc.impls.workloads.ldbc.snb.interactive.InteractiveTest;
 import com.ldbc.impls.workloads.ldbc.snb.postgres.interactive.PostgresInteractiveDb;
+import org.junit.Ignore;
 
 import java.util.Map;
 
+@Ignore
 public class PostgresInteractiveTest extends InteractiveTest implements PostgresSnbTest {
 
     public PostgresInteractiveTest() {
