19d2ad393e706b2e0cdbd065903b8cb35c93128c
Gabor Szarnyas <szarnyasg@gmail.com>
2020-12-18 20:37:31 +0100

    Add checks and pv to Cypher script

--- cypher/scripts/convert-csvs.sh
+++ cypher/scripts/convert-csvs.sh
@@ -4,6 +4,16 @@ echo "Starting preprocessing CSV files"
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+: ${NEO4J_CSV_DIR:?"Environment variable NEO4J_CSV_DIR is unset or empty"}
+: ${NEO4J_CSV_POSTFIX:?"Environment variable NEO4J_CSV_POSTFIX is unset or empty"}
+
+# provide progressbar is available
+if command -v pv > /dev/null 2>&1; then
+  SNB_CAT=pv
+else
+  SNB_CAT=cat
+fi
+
 # replace headers
 while read line; do
   IFS=' ' read -r -a array <<< $line
@@ -12,7 +22,7 @@ while read line; do
 
   echo ${FILENAME}: ${HEADER}
   # replace header (no point using sed to save space as it creates a temporary file as well)
-  echo ${HEADER} | cat - <(tail -n +2 ${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX}) > tmpfile.csv && mv tmpfile.csv ${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX}
+  echo ${HEADER} | ${SNB_CAT} - <(tail -n +2 ${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX}) > tmpfile.csv && mv tmpfile.csv ${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX}
 done < headers.txt
 
 echo "Finished preprocessing CSV files"
