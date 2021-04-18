07d1c06cc0a6c1b1f55387c8ce3cf38594ff3c21
Gabor Szarnyas <szarnyasg@gmail.com>
2021-04-18 23:16:45 +0200

    Cast number to integer in IC Q7. Fixes #161

--- postgres/queries/interactive-complex-7.sql
+++ postgres/queries/interactive-complex-7.sql
@@ -1,6 +1,6 @@
 select p_personid, p_firstname, p_lastname, l.l_creationdate, m_messageid,
 	COALESCE(m_ps_imagefile,'')||COALESCE(m_content,''),
-	EXTRACT(EPOCH FROM (l.l_creationdate - m_creationdate)) / 60 as lag,
+	CAST(EXTRACT(EPOCH FROM (l.l_creationdate - m_creationdate)) AS INTEGER) / 60 as minutesLatency,
     (case when exists (select 1 from knows where k_person1id = :personId and k_person2id = p_personid) then 0 else 1 end) as isnew
 from
   (select l_personid, max(l_creationdate) as l_creationdate
