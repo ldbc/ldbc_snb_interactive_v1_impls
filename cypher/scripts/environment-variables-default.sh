6af414751a5767538b84c51b56caf3e329b6cd1e
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-22 21:43:11 +0200

    Rename 'neo4j-scratch' directory to 'scratch' for consistency

--- cypher/scripts/environment-variables-default.sh
+++ cypher/scripts/environment-variables-default.sh
@@ -1,7 +1,7 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-export NEO4J_CONTAINER_ROOT=`pwd`/neo4j-scratch
+export NEO4J_CONTAINER_ROOT=`pwd`/scratch
 export NEO4J_DATA_DIR=${NEO4J_CONTAINER_ROOT}/data
 export NEO4J_VERSION=4.3.0
 export NEO4J_ENV_VARS=""
