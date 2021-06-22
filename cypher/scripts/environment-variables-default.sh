81b8f0540b3bd9885fd6e2afb7f813ce45fbe7d2
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-22 15:12:20 +0200

    Adjust container name for Neo4j/Cypher

--- cypher/scripts/environment-variables-default.sh
+++ cypher/scripts/environment-variables-default.sh
@@ -7,4 +7,4 @@ export NEO4J_VERSION=4.3.1
 export NEO4J_ENV_VARS=""
 export NEO4J_CSV_DIR=`pwd`/test-data/converted
 export NEO4J_CSV_POSTFIX=_0_0.csv
-export NEO4J_CONTAINER_NAME="neo-snb"
+export NEO4J_CONTAINER_NAME="snb-neo4j"
