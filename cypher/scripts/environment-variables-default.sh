cb9af0952b0c695f477ff58abc6179edae305b38
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-23 22:10:18 +0200

    Make container names unique among SNB workloads

--- cypher/scripts/environment-variables-default.sh
+++ cypher/scripts/environment-variables-default.sh
@@ -8,4 +8,4 @@ export NEO4J_ENV_VARS=""
 export NEO4J_VANILLA_CSV_DIR=`pwd`/test-data/vanilla
 export NEO4J_CONVERTED_CSV_DIR=`pwd`/test-data/converted
 export NEO4J_CSV_POSTFIX=_0_0.csv
-export NEO4J_CONTAINER_NAME="snb-neo4j"
+export NEO4J_CONTAINER_NAME=snb-interactive-neo4j
