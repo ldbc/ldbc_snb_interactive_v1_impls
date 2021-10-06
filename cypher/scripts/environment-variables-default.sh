082eed7096491d4d6225f88e1899b800b44e595c
Gabor Szarnyas <szarnyasg@gmail.com>
2021-10-06 11:27:49 +0200

    Bump Neo4j version

--- cypher/scripts/environment-variables-default.sh
+++ cypher/scripts/environment-variables-default.sh
@@ -3,7 +3,7 @@ cd ..
 
 export NEO4J_CONTAINER_ROOT=`pwd`/scratch
 export NEO4J_DATA_DIR=${NEO4J_CONTAINER_ROOT}/data
-export NEO4J_VERSION=4.3.4
+export NEO4J_VERSION=4.3.5
 export NEO4J_ENV_VARS=""
 export NEO4J_VANILLA_CSV_DIR=`pwd`/test-data/vanilla
 export NEO4J_CONVERTED_CSV_DIR=`pwd`/test-data/converted
