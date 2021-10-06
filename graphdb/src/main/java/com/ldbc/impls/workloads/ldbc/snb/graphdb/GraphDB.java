713162fc4f36f477029bf2e7d6dfcbb804f4ee33
Diana Ivanova <diana.ivanova@ontotext.com>
2021-10-06 11:50:53 +0300

    - renamed long queries

--- graphdb/src/main/java/com/ldbc/impls/workloads/ldbc/snb/graphdb/GraphDB.java
+++ graphdb/src/main/java/com/ldbc/impls/workloads/ldbc/snb/graphdb/GraphDB.java
@@ -4,6 +4,8 @@ import com.ldbc.driver.DbException;
 import com.ldbc.driver.control.LoggingService;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery1;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery1Result;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery8;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery8Result;
 import com.ldbc.impls.workloads.ldbc.snb.db.BaseDb;
 import com.ldbc.impls.workloads.ldbc.snb.graphdb.operationhandlers.GraphDBListOperationHandler;
 import org.eclipse.rdf4j.model.IRI;
@@ -35,4 +37,25 @@ public class GraphDB extends BaseDb<GraphDBQueryStore> {
 			return null;
 		}
 	}
+
+
+	// public static class InteractiveQuery8 extends GraphDBListOperationHandler<LdbcQuery8, LdbcQuery8Result> {
+	//
+	// 	@Override
+	// 	public String getQueryString(GraphDBConnectionState state, LdbcQuery8 operation) {
+	// 		return state.getQueryStore().getQuery8(operation);
+	// 	}
+	//
+	// 	@Override
+	// 	public LdbcQuery8Result convertSingleResult(BindingSet bindingSet) {
+	// 		return new LdbcQuery8Result(
+	// 				Long.parseLong(bindingSet.getBinding("personId").getValue().stringValue()),
+	// 				result.getString(2),
+	// 				result.getString(3),
+	// 				PostgresConverter.stringTimestampToEpoch(result, 4),
+	// 				Long.parseLong(bindingSet.getBinding("personId").getValue().stringValue()),
+	// 				result.getString(6));
+	// 	}
+	// 	}
+	//}
 }
