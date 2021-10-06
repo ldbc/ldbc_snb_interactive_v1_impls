173b495995b0d32174a276ecb66d9a3443725409
denitsa <denitsa.stoyanova@ontotext.com>
2021-10-06 15:41:41 +0300

    Add long and collection converters

--- graphdb/src/main/java/com/ldbc/impls/workloads/ldbc/snb/graphdb/converter/GraphDBConverter.java
+++ graphdb/src/main/java/com/ldbc/impls/workloads/ldbc/snb/graphdb/converter/GraphDBConverter.java
@@ -1,8 +1,12 @@
 package com.ldbc.impls.workloads.ldbc.snb.graphdb.converter;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.ldbc.impls.workloads.ldbc.snb.converter.Converter;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.impl.SimpleIRI;
 
 public class GraphDBConverter extends Converter {
 
@@ -12,4 +16,19 @@ public class GraphDBConverter extends Converter {
 		Instant instant = Instant.parse (date);
 		return instant.toEpochMilli();
 	}
+
+	public Iterable<String> convertToStringCollection(Value value) {
+		if (value == null) {
+			return new ArrayList<>();
+		} else {
+			return Arrays.asList(value.stringValue().split(", "));
+		}
+	}
+
+	public long asLong(Value value) {
+		int numberIndex = 4;
+		String localName = ((SimpleIRI) value).getLocalName();
+		return Long.parseLong(localName.substring(numberIndex));
+	}
+
 }
