fef50e3294d6c6c543382c067aa83315713f5c1e
denitsa <denitsa.stoyanova@ontotext.com>
2021-10-06 09:41:23 +0300

    Fix benchmark script

--- graphdb/driver/benchmark.sh
+++ graphdb/driver/benchmark.sh
@@ -6,4 +6,4 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-java -cp jeeves-standalone.jar com.ldbc.driver.Client -P driver/benchmark.properties
+java -cp target/jeeves-standalone.jar com.ldbc.driver.Client -P driver/benchmark.properties
