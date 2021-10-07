adb0b87f89d7457df1b9df047cc341249bfdfc13
Diana Ivanova <diana.ivanova@ontotext.com>
2021-10-07 12:48:09 +0300

    - Added query 10 handler. - query 10 parameters fixed

--- graphdb/src/main/java/com/ldbc/impls/workloads/ldbc/snb/graphdb/interactive/GraphDBInteractive.java
+++ graphdb/src/main/java/com/ldbc/impls/workloads/ldbc/snb/graphdb/interactive/GraphDBInteractive.java
@@ -22,7 +22,7 @@ public class GraphDBInteractive extends GraphDB {
 //		registerOperationHandler(LdbcQuery7.class, InteractiveQuery7.class);
 		registerOperationHandler(LdbcQuery8.class, InteractiveQuery8.class);
 		registerOperationHandler(LdbcQuery9.class, InteractiveQuery9.class);
-//		registerOperationHandler(LdbcQuery10.class, InteractiveQuery10.class);
+		registerOperationHandler(LdbcQuery10.class, InteractiveQuery10.class);
 //		registerOperationHandler(LdbcQuery11.class, InteractiveQuery11.class);
 //		registerOperationHandler(LdbcQuery12.class, InteractiveQuery12.class);
 //		registerOperationHandler(LdbcQuery13.class, InteractiveQuery13.class);
