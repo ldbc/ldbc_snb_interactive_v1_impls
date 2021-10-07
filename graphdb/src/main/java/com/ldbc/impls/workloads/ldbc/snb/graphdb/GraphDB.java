89a3e93b21b8fa577567971eeb22f3108707c654
denitsa <denitsa.stoyanova@ontotext.com>
2021-10-07 12:33:26 +0300

    Add interactive complex query2

--- graphdb/src/main/java/com/ldbc/impls/workloads/ldbc/snb/graphdb/GraphDB.java
+++ graphdb/src/main/java/com/ldbc/impls/workloads/ldbc/snb/graphdb/GraphDB.java
@@ -2,18 +2,14 @@ package com.ldbc.impls.workloads.ldbc.snb.graphdb;
 
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.control.LoggingService;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery1;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery1Result;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery8;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery8Result;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery9;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery9Result;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcUpdate2AddPostLike;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.*;
 import com.ldbc.impls.workloads.ldbc.snb.db.BaseDb;
 import com.ldbc.impls.workloads.ldbc.snb.graphdb.converter.GraphDBConverter;
 import com.ldbc.impls.workloads.ldbc.snb.graphdb.operationhandlers.GraphDBListOperationHandler;
 import com.ldbc.impls.workloads.ldbc.snb.graphdb.operationhandlers.GraphDBUpdateOperationHandler;
 
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
 import org.eclipse.rdf4j.query.BindingSet;
 
 import java.util.List;
@@ -52,6 +48,23 @@ public class GraphDB extends BaseDb<GraphDBQueryStore> {
 					cnv.asString(bs, names.get(10)),
 					cnv.asObjectCollection(bs, names.get(11)),
 					cnv.asObjectCollection(bs, names.get(12)));
+		}
+	}
+
+	public static class InteractiveQuery2 extends GraphDBListOperationHandler<LdbcQuery2, LdbcQuery2Result> {
+		@Override
+		public String getQueryString(GraphDBConnectionState state, LdbcQuery2 operation) {
+			return state.getQueryStore().getQuery2(operation);
+		}
+
+		@Override
+		public LdbcQuery2Result convertSingleResult(List<String> names, BindingSet bs) {
+			return new LdbcQuery2Result(cnv.asLong(bs, names.get(0)),
+					cnv.asString(bs, names.get(1)),
+					cnv.asString(bs, names.get(2)),
+					cnv.asLong(bs, names.get(3)),
+					cnv.asString(bs, names.get(4)),
+					cnv.timestampToEpoch(bs, names.get(5)));
 		}
 	}
 
