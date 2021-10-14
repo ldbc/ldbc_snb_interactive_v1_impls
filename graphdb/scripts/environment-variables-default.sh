15b4798fea565a63291ba575b0f162df5ff17d31
Diana Ivanova <diana.ivanova@ontotext.com>
2021-10-14 11:48:26 +0300

    - add preload, start and stop scripts - add preload container name preload

--- graphdb/scripts/environment-variables-default.sh
+++ graphdb/scripts/environment-variables-default.sh
@@ -7,4 +7,5 @@ export GRAPHDB_TTL_DIR=`pwd`/test-data/
 export REPOSITORY_CONFIG_FILE=`pwd`/config/graphdb-repo-config.ttl
 export GRAPHDB_VERSION=9.9.1
 export GRAPHDB_CONTAINER_NAME=snb-interactive-graphdb
+export PRELOAD_CONTAINER_NAME=preload-graphdb
 export GRAPHDB_PORT = 7200
