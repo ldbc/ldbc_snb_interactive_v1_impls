941809dd347ad5fa19cd25987c38314a92ec89d3
denitsa <denitsa.stoyanova@ontotext.com>
2021-10-14 18:02:06 +0300

    Fix starting graphdb scripts

--- graphdb/scripts/graphdb-preload.sh
+++ graphdb/scripts/graphdb-preload.sh
@@ -6,5 +6,4 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ../config
 
-docker-compose -f docker-compose-preload.yml build
 docker-compose -f docker-compose-preload.yml up -d
