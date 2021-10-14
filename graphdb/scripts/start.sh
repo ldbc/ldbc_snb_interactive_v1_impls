78a3e22e3df3348cb3323f5b25e39f590fb1901c
Diana Ivanova <diana.ivanova@ontotext.com>
2021-10-14 11:48:18 +0300

    - add preload, start and stop scripts - add preload container name preload

---
+++ graphdb/scripts/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+cd ../config
+
+docker-compose up -f docker-compose-start.yml -d
