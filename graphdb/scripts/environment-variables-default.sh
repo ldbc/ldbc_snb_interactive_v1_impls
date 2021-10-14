c90330ecff022abb3a697887461f82be1ea1e777
denitsa <denitsa.stoyanova@ontotext.com>
2021-10-14 11:32:09 +0300

    Add repository config file

---
+++ graphdb/scripts/environment-variables-default.sh
@@ -0,0 +1,10 @@
+cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+export GRAPHDB_CONTAINER_ROOT=`pwd`/scratch
+export GRAPHDB_DATA_DIR=${GRAPHDB_CONTAINER_ROOT}/data
+export GRAPHDB_TTL_DIR=`pwd`/test-data/
+export REPOSITORY_CONFIG_FILE=`pwd`/config/graphdb-repo-config.ttl
+export GRAPHDB_VERSION=9.9.1
+export GRAPHDB_CONTAINER_NAME=snb-interactive-graphdb
+export GRAPHDB_PORT = 7200
