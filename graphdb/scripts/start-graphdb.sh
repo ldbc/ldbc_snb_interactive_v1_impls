5a7d824fcdb291c76a2927237ed79b1e4d7e6fbf
denitsa <denitsa.stoyanova@ontotext.com>
2021-10-15 22:11:21 +0300

    Add environment variables checks in scripts

--- graphdb/scripts/start-graphdb.sh
+++ graphdb/scripts/start-graphdb.sh
@@ -5,6 +5,14 @@ set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+: ${GRAPHDB_CONTAINER_ROOT:?"Environment variable GRAPHDB_CONTAINER_ROOT is unset or empty"}
+: ${GRAPHDB_HEAP_SIZE:?"Environment variable GRAPHDB_HEAP_SIZE is unset or empty"}
+: ${GRAPHDB_VERSION:?"Environment variable GRAPHDB_VERSION is unset or empty"}
+: ${GRAPHDB_CONTAINER_NAME:?"Environment variable GRAPHDB_CONTAINER_NAME is unset or empty"}
+
 cd ../config
 
+echo -n "Waiting for the database to start..."
 docker-compose -f docker-compose-start.yml up -d
+echo
+echo "GraphDB has started successfully"
