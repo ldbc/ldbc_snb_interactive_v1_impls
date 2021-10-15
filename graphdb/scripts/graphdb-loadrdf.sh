23ae81b06e711eee9013910a228a1332bba1786b
Diana Ivanova <diana.ivanova@ontotext.com>
2021-10-15 15:30:21 +0300

    - remove detached mode from docker

--- graphdb/scripts/graphdb-loadrdf.sh
+++ graphdb/scripts/graphdb-loadrdf.sh
@@ -6,4 +6,4 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ../config
 
-docker-compose -f docker-compose-loadrdf.yml up -d
+docker-compose -f docker-compose-loadrdf.yml up
