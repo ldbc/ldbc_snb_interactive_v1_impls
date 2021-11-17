ee8b8007bbb519750cf0ae75924335d16363ea23
Gabor Szarnyas <szarnyasg@gmail.com>
2021-11-17 14:15:15 +0100

    Postgres: bump version, spell out arguments, add connect script

---
+++ postgres/scripts/connect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. scripts/vars.sh
+
+docker exec -it ${POSTGRES_CONTAINER_NAME} psql --username=${POSTGRES_USER} --dbname=${POSTGRES_DATABASE}
