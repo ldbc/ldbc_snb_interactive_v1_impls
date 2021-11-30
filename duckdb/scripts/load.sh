1d8d0796329cb6df6a97f12a8d1739b05355db15
Gabor Szarnyas <szarnyasg@gmail.com>
2021-11-30 18:48:13 +0100

    Omit applying schema contraints in the load script as these are not persisted by DuckDB

--- duckdb/scripts/load.sh
+++ duckdb/scripts/load.sh
@@ -10,4 +10,3 @@ scripts/cleanup.sh
 
 cat ddl/schema.sql | scratch/duckdb scratch/ldbc.duckdb
 sed "s|PATHVAR|${DUCKDB_DATA_DIR}|" ddl/snb-load.sql | scratch/duckdb scratch/ldbc.duckdb
-cat ddl/schema_constraints.sql | scratch/duckdb scratch/ldbc.duckdb
