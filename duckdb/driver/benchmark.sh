7a2bea6b66488dc2cef134619495d1c8d03b9682
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-05 20:25:37 +0100

    Allow specifying custom properties files as arguments to the driver scripts

--- duckdb/driver/benchmark.sh
+++ duckdb/driver/benchmark.sh
@@ -6,4 +6,6 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-java -cp target/duckdb-0.3.6-SNAPSHOT.jar com.ldbc.driver.Client -P driver/benchmark.properties
+BENCHMARK_PROPERTIES_FILE=${1:-driver/benchmark.properties}
+
+java -cp target/duckdb-0.3.6-SNAPSHOT.jar com.ldbc.driver.Client -P ${BENCHMARK_PROPERTIES_FILE}
--- duckdb/driver/create-validation-parameters.sh
+++ duckdb/driver/create-validation-parameters.sh
@@ -6,4 +6,6 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-java -cp target/duckdb-0.3.6-SNAPSHOT.jar com.ldbc.driver.Client -P driver/create-validation-parameters.properties
+CREATE_VALIDATION_PARAMETERS_PROPERTIES_FILE=${1:-driver/create-validation-parameters.properties}
+
+java -cp target/duckdb-0.3.6-SNAPSHOT.jar com.ldbc.driver.Client -P ${CREATE_VALIDATION_PARAMETERS_PROPERTIES_FILE}
