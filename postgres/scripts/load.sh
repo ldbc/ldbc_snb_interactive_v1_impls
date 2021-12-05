566438c0df6d54aba830b2a975f8eccde3d28ad3
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-05 19:59:04 +0100

    Load comments table as-is and use SQL to create messages table
    Rather than relying on CLI tools such as AWK as before
    (when both comment_*.csv and post_*.csv were directly loaded to the same
    table).

--- postgres/scripts/load.sh
+++ postgres/scripts/load.sh
@@ -13,17 +13,4 @@ if [ ! -d "${POSTGRES_CSV_DIR}" ]; then
     exit 1
 fi
 
-POSTGRES_FORCE_REGENERATE=${POSTGRES_FORCE_REGENERATE:-no}
-
-# we regenerate PostgreSQL-specific CSV files for comments, if either
-#  - it doesn't exist
-#  - the source CSV is newer
-#  - we are forced to do so by environment variable POSTGRES_FORCE_REGENERATE=yes
-
-if [ ! -f ${POSTGRES_CSV_DIR}/dynamic/comment_0_0-postgres.csv -o ${POSTGRES_CSV_DIR}/dynamic/comment_0_0.csv -nt ${POSTGRES_CSV_DIR}/dynamic/comment_0_0-postgres.csv -o "${POSTGRES_FORCE_REGENERATE}x" = "yesx" ]; then
-    cat ${POSTGRES_CSV_DIR}/dynamic/comment_0_0.csv | \
-        awk -F '|' '{print $1"||"$2"|"$3"|"$4"||"$5"|"$6"|"$7"|"$8"||"$9 $10}' > \
-        ${POSTGRES_CSV_DIR}/dynamic/comment_0_0-postgres.csv
-fi
-
 python3 load.py
