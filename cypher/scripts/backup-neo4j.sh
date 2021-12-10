c38db6983ac1d3ee30e5d7e02ea70505dbcf7eda
James C <jclarkson@users.noreply.github.com>
2021-12-10 10:11:41 +0000

    Cypher: Added backup/restore scripts
    Using these scripts a user only needs to import from CSV files once.

---
+++ cypher/scripts/backup-neo4j.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+: ${NEO4J_CONTAINER_ROOT:?"Environment variable NEO4J_CONTAINER_ROOT is unset or empty"}
+: ${NEO4J_DATA_DIR:?"Environment variable NEO4J_DATA_DIR is unset or empty"}
+: ${NEO4J_VERSION:?"Environment variable NEO4J_VERSION is unset or empty"}
+: ${NEO4J_CONTAINER_NAME:?"Environment variable NEO4J_CONTAINER_NAME is unset or empty"}
+
+if [ ! -d "${NEO4J_DATA_DIR}" ]; then
+    echo "Neo4j data directory does not exist"
+    exit 1
+fi
+
+DUMP_FILE=$1
+if [ -z "${DUMP_FILE}" ]; then
+  echo "Usage: $0 filename"
+  exit 1
+fi
+
+docker run --interactive --tty --rm \
+    --user="$(id -u):$(id -g)" \
+    --volume="${NEO4J_DATA_DIR}":/data \
+    --volume="${NEO4J_CONTAINER_ROOT}/conf":/conf \
+    --volume="${NEO4J_CONTAINER_ROOT}/backups":/backups \
+    --name "${NEO4J_CONTAINER_NAME}" \
+    "neo4j:${NEO4J_VERSION}" \
+    neo4j-admin dump --database=neo4j --to="/backups/${DUMP_FILE}"
