2503efb98a1e65af557d37728fdd1461904af983
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-12 09:03:46 +0100

    Add backup and restore scripts to DuckDB

---
+++ duckdb/scripts/backup-database.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. scripts/vars.sh
+
+cp scratch/ldbc.duckdb scratch/ldbc.duckdb.bkp
