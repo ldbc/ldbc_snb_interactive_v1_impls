b757e4f2493f232d3c56d1cdc2991ddc374d2078
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-16 11:08:14 +0100

    Set u flag in all Bash scripts

--- cypher/driver/benchmark.sh
+++ cypher/driver/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eu
 set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
--- cypher/driver/determine-best-tcr.sh
+++ cypher/driver/determine-best-tcr.sh
@@ -2,7 +2,7 @@
 
 # Script to determine the best TCR (total_compression_ratio) value.
 
-set -e
+set -eu
 set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
