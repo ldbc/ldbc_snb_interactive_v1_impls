1f6c333d9a125923f72827ea8e2467ffa0da0a92
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-16 09:31:55 +0100

    Ensure that the Neo4j dependency installer script grabs the correct version of the package

--- cypher/scripts/install-dependencies.sh
+++ cypher/scripts/install-dependencies.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-pip3 install -U neo4j
+set -e
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. scripts/vars.sh
+
+pip3 install --user neo4j==${NEO4J_VERSION} python-dateutil
