b6c348c6a333be3ff0b39a9b20204b07b0ea13a7
Gabor Szarnyas <szarnyasg@gmail.com>
2022-01-03 18:03:27 +0100

    Remove unused file

--- umbra/my.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-con = psycopg2.connect(host="localhost", user="postgres", password="mysecretpassword", port=5432)
-
-cur = con.cursor()
-cur.execute("select 42 as x")
-res = cur.fetchall()
-print(res)
