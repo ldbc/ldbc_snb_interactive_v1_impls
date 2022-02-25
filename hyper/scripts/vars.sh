0f419996d19fbf1c2413ba8b535e385f0cab6a63
Gabor Szarnyas <szarnyasg@gmail.com>
2022-02-25 20:44:27 +0100

    Adjust Hyper vars

--- hyper/scripts/vars.sh
+++ hyper/scripts/vars.sh
@@ -1,10 +1,8 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-export HYPER_VERSION=14.1
-export HYPER_CONTAINER_NAME=snb-interactive-postgres
-export HYPER_PASSWORD=mysecretpassword
+export HYPER_PASSWORD=
 export HYPER_DATABASE=ldbcsnb
-export HYPER_USER=postgres
+export HYPER_USER=ldbcsnb
 export HYPER_DATA_DIR=`pwd`/scratch/data
-export HYPER_PORT=5432
+export HYPER_PORT=7484
