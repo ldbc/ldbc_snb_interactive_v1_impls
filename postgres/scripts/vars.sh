fc34b46443f9cd717a86aad6975edc0ee57c6fe0
Gabor Szarnyas <szarnyasg@gmail.com>
2022-02-25 20:44:17 +0100

    Restore Postgres scripts

--- postgres/scripts/vars.sh
+++ postgres/scripts/vars.sh
@@ -1,8 +1,10 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-export POSTGRES_PASSWORD=
+export POSTGRES_VERSION=14.1
+export POSTGRES_CONTAINER_NAME=snb-interactive-postgres
+export POSTGRES_PASSWORD=mysecretpassword
 export POSTGRES_DATABASE=ldbcsnb
-export POSTGRES_USER=ldbcsnb
+export POSTGRES_USER=postgres
 export POSTGRES_DATA_DIR=`pwd`/scratch/data
-export POSTGRES_PORT=7484
+export POSTGRES_PORT=5432
