2235f95eece36ee154113e6ae4528b92ba6384ff
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-14 15:31:30 +0100

    Add common 'install-dependencies.sh' script

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+cypher/scripts/install-dependencies.sh
+postgres/scripts/install-dependencies.sh
