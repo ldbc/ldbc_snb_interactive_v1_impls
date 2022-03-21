0ae714ae9ce9440cda9dc6f8b0e4d9557c01b4f6
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-21 11:25:13 +0100

    Bump container version

--- umbra/scripts/vars.sh
+++ umbra/scripts/vars.sh
@@ -5,4 +5,4 @@ export UMBRA_DDL_DIR=`pwd`/ddl/
 export UMBRA_DATABASE_DIR=`pwd`/scratch/db/
 export UMBRA_BACKUP_DIR=`pwd`/scratch/backup/
 export UMBRA_CONTAINER_NAME=snb-interactive-umbra
-export UMBRA_DOCKER_IMAGE=umbra-release:533b93fd2
+export UMBRA_DOCKER_IMAGE=umbra-release:e7415d13f
