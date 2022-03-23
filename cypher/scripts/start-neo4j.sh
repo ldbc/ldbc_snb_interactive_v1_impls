8656427e310ee2363d946d0ae308fb113961b68d
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-24 00:40:39 +0100

    Add ulimit setting to Neo4j container command

--- cypher/scripts/start-neo4j.sh
+++ cypher/scripts/start-neo4j.sh
@@ -18,6 +18,7 @@ docker run --rm \
     --publish=7474:7474 \
     --publish=7687:7687 \
     --detach \
+    --ulimit nofile=40000:40000 \
     ${NEO4J_ENV_VARS} \
     --volume=${NEO4J_DATA_DIR}:/data:z \
     --volume=${NEO4J_CONTAINER_ROOT}/logs:/logs:z \
