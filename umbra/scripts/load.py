c75fdec25e8ce9f39e91689f77d598789d48e39a
David Puroja <dpuroja@outlook.com>
2022-04-04 15:38:21 +0200

    Remove umbra-container folder. Clean python load script.

--- umbra/scripts/load.py
+++ umbra/scripts/load.py
@@ -1,14 +1,7 @@
 import psycopg2
 import sys
-import os
 import re
 import time
-
-# def vacuum(con):
-#     old_isolation_level = con.isolation_level
-#     pg_con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-#     pg_con.cursor().execute("VACUUM FULL")
-#     pg_con.set_isolation_level(old_isolation_level)
 
 if len(sys.argv) < 2:
     print("Umbra loader script")
@@ -38,54 +31,12 @@ def run_script(con, filename):
             duration = end - start
             print(f"-> {duration:.4f} seconds")
 
-
-# run_script(con, "ddl/drop-views.sql")
-# run_script(con, "ddl/drop-tables.sql")
-# run_script(con, "ddl/schema.sql")
-
 print("Load initial snapshot")
 run_script(con, "ddl/load.sql")
-
-# # initial snapshot
-# static_path = f"{data_dir}/static"
-# dynamic_path = f"{data_dir}/dynamic"
-# # static_entities = ["Organisation", "Place", "Tag", "TagClass"]
-# # dynamic_entities = ["Comment", "Comment_hasTag_Tag", "Forum", "Forum_hasMember_Person", "Forum_hasTag_Tag", "Person", "Person_hasInterest_Tag", "Person_knows_Person", "Person_likes_Comment", "Person_likes_Post", "Person_studyAt_University", "Person_workAt_Company", "Post", "Post_hasTag_Tag"]
-
-# if local:
-#     dbs_data_dir = data_dir
-# else:
-#     dbs_data_dir = '/data'
-
-# print("## Static entities")
-# # for entity in static_entities:
-# for csv_file in [f for f in os.listdir(f"{static_path}") if f.startswith("part-") and f.endswith(".csv")]:
-#     csv_path = f"{entity}/{csv_file}"
-#     print(f"- {csv_path}")
-#     #print(f"- {csv_path}", end='\r')
-#     con.execute(f"COPY {entity} FROM '{dbs_data_dir}/static/{entity}/{csv_file}' (DELIMITER '|', HEADER, FORMAT csv)")
-#     #print(" " * 120, end='\r')
-#     pg_con.commit()
-# print("Loaded static entities.")
-
-# print("## Dynamic entities")
-# # for entity in dynamic_entities:
-# for csv_file in [f for f in os.listdir(f"{dynamic_path}/{entity}") if f.startswith("part-") and f.endswith(".csv")]:
-#     csv_path = f"{entity}/{csv_file}"
-#     print(f"- {csv_path}")
-#     #print(f"- {csv_path}", end='\r')
-#     con.execute(f"COPY {entity} FROM '{dbs_data_dir}/dynamic/{entity}/{csv_file}' (DELIMITER '|', HEADER, FORMAT csv)")
-#     #print(" " * 120, end='\r')
-#     pg_con.commit()
-# print("Loaded dynamic entities.")
 
 print("Creating materialized views . . . ")
 run_script(con, "ddl/schema_constraints.sql")
 pg_con.commit()
 print("Done.")
-
-# print("Vacuuming . . . ")
-# vacuum(pg_con)
-# print("Done.")
 
 print("Loaded initial snapshot to Umbra.")
