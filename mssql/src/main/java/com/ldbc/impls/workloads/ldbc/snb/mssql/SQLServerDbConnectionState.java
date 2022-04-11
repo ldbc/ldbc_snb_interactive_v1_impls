4986eba76615fbf81906f60704de247b50057874
David Puroja <dpuroja@outlook.com>
2022-04-11 10:26:10 +0200

    Change SQL Server client driver to HikariCP

--- mssql/src/main/java/com/ldbc/impls/workloads/ldbc/snb/mssql/SQLServerDbConnectionState.java
+++ mssql/src/main/java/com/ldbc/impls/workloads/ldbc/snb/mssql/SQLServerDbConnectionState.java
@@ -2,33 +2,44 @@ package com.ldbc.impls.workloads.ldbc.snb.mssql;
 
 import com.ldbc.driver.DbException;
 import com.ldbc.impls.workloads.ldbc.snb.BaseDbConnectionState;
+
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.util.Properties;
 import java.util.Map;
 import java.util.TimeZone;
-import java.sql.DriverManager;
+
 public class SQLServerDbConnectionState extends BaseDbConnectionState<SQLServerQueryStore> {
 
     protected String endPoint;
     protected Connection connection;
-    private String connectionString;
+    protected HikariDataSource ds;
 
     public SQLServerDbConnectionState(Map<String, String> properties, SQLServerQueryStore store) throws ClassNotFoundException {
         super(properties, store);
 
+        Properties props = new Properties();
         endPoint = properties.get("endpoint");
-        connectionString = endPoint 
-                             + ";databaseName=" + properties.get("databaseName")
-                             + ";user=" + properties.get("user")
-                             + ";password=" + properties.get("password")
-                             + ";encrypt=false";
+        props.setProperty("dataSource.databaseName", properties.get("databaseName"));
+        props.setProperty("dataSource.encrypt", "false");
+
+        HikariConfig config = new HikariConfig(props);
+        config.setPassword(properties.get("password"));
+        config.setUsername(properties.get("user"));
+        config.setJdbcUrl(endPoint);
+        ds = new HikariDataSource(config);
+        // ds.setDataSourceClassName("com.microsoft.sqlserver.jdbc.SQLServerDataSource");
     }
 
     public Connection getConnection() throws DbException {
         try {
             if (connection == null) {
                 TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
-                connection = DriverManager.getConnection(connectionString);
+                connection = ds.getConnection();
             }
         } catch (SQLException e) {
             throw new DbException(e);
