d3de117e4398c6c2f6d7e11485550e2c94940983
Gabor Szarnyas <szarnyasg@gmail.com>
2022-04-25 12:50:43 +0200

    Ensure that Q7 implementations truncate fractional seconds
    Q7 needs to report the minutes passed -- this should be done without any
    rounding, i.e. 60000 milliseconds must pass, 59999 milliseconds are
    insufficient.
    
    By default, Postgres rounds a number when casting a float to an integer.
    This commit add 'floor' to avoid to ensure the number is truncated.
    
    The Neo4j/Cypher implementation was changed: it previously used an
    explicit 'round' function and now uses 'floor'.
    
    The investigation of this problem led to the following bug report:
    https://github.com/duckdb/duckdb/issues/3497

--- postgres/queries/interactive-complex-7.sql
+++ postgres/queries/interactive-complex-7.sql
@@ -3,7 +3,7 @@
  */
 select p_personid, p_firstname, p_lastname, l.l_creationdate, m_messageid,
     COALESCE(m_ps_imagefile, m_content),
-    CAST(EXTRACT(EPOCH FROM (l.l_creationdate - m_creationdate)) AS INTEGER) / 60 as minutesLatency,
+    CAST(floor(EXTRACT(EPOCH FROM (l.l_creationdate - m_creationdate))) AS INTEGER) / 60 as minutesLatency,
     (case when exists (select 1 from knows where k_person1id = :personId and k_person2id = p_personid) then 0 else 1 end) as isnew
 from
   (select l_personid, max(l_creationdate) as l_creationdate
