cdd0399b8371e307ee3c12fbec3617346c78d3af
Gabor Szarnyas <szarnyasg@gmail.com>
2022-05-05 12:23:07 +0200

    Adjust DuckDB's get script to support MacOS/M1

--- duckdb/scripts/get.sh
+++ duckdb/scripts/get.sh
@@ -8,7 +8,19 @@ cd ..
 
 . scripts/vars.sh
 
+if [ "$(uname)" == "Darwin" ]; then
+  OS=osx
+else
+  OS=linux
+fi
+
+if [[ `uname -m` == "arm64" ]]; then
+    ARCH=??
+else
+    ARCH=amd64
+fi
+
 cd scratch
-wget -q https://github.com/duckdb/duckdb/releases/download/v${DUCKDB_VERSION}/duckdb_cli-linux-amd64.zip -O duckdb_cli-linux-amd64.zip
-unzip -o duckdb_cli-linux-amd64.zip
-rm duckdb_cli-linux-amd64.zip
+wget -q https://github.com/duckdb/duckdb/releases/download/v${DUCKDB_VERSION}/duckdb_cli-${OS}-${ARCH}.zip -O duckdb_cli-${OS}-${ARCH}.zip
+unzip -o duckdb_cli-${OS}-${ARCH}.zip
+rm duckdb_cli-${OS}-${ARCH}.zip
