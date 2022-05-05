ec866aac1d2cd4de5ab969ce791c1c546a528070
David Puroja <dpuroja@outlook.com>
2022-05-05 16:15:07 +0200

    Fix Q12

--- mssql/queries/interactive-complex-12.sql
+++ mssql/queries/interactive-complex-12.sql
@@ -1,23 +1,44 @@
-with extended_tags(s_subtagclassid,s_supertagclassid) as (
-    select tc_tagclassid, tc_tagclassid from tagclass
-    UNION ALL
-    select tc.tc_tagclassid, t.s_supertagclassid from tagclass tc, extended_tags t
-        where tc.tc_subclassoftagclassid=t.s_subtagclassid
+WITH extended_tags(s_subtagclassid,s_supertagclassid) AS (
+    SELECT tc_tagclassid
+         , tc_tagclassid 
+      FROM tagclass
+     UNION ALL
+    SELECT tc.tc_tagclassid
+         , t.s_supertagclassid
+      FROM tagclass tc
+         , extended_tags t
+     WHERE tc.tc_subclassoftagclassid = t.s_subtagclassid
 )
-select top(20) p_personid, p_firstname, p_lastname, string_agg(t_name, ';'), count(*) AS replyCount
-from person, message p1, knows, message p2, message_tag, 
-    (select distinct t_tagid, t_name from tag where (t_tagclassid in (
-          select distinct s_subtagclassid from extended_tags k, tagclass
-        where tc_tagclassid = k.s_supertagclassid and tc_name = :tagClassName) 
-   )) selected_tags
-where
-  k_person1id = :personId and 
-  k_person2id = p_personid and 
-  p_personid = p1.m_creatorid and 
-  p1.m_c_replyof = p2.m_messageid and 
-  p2.m_c_replyof is null and
-  p2.m_messageid = mt_messageid and 
-  mt_tagid = t_tagid
-group by p_personid, p_firstname, p_lastname
-order by replyCount desc, p_personid
-;
+SELECT TOP(20) p_personid
+             , p_firstname
+             , p_lastname
+             , string_agg(t_name, ';')
+             , sum(partialReplyCount)
+            AS replyCount
+          FROM ( SELECT DISTINCT p_personid, p_firstname, p_lastname, t_name, count(*) AS partialReplyCount
+                              FROM person
+                                 , message p1
+                                 , knows
+                                 , message p2
+                                 , message_tag
+                                 , (SELECT DISTINCT t_tagid
+                                                   , t_name 
+                                                FROM tag
+                                             WHERE (t_tagclassid IN (
+                                                      SELECT DISTINCT s_subtagclassid
+                                                               FROM extended_tags k
+                                                                  , tagclass
+                                                               WHERE tc_tagclassid = k.s_supertagclassid 
+                                                                  AND tc_name = :tagClassName) 
+                                                   )) selected_tags
+                              WHERE k_person1id = :personId
+                              AND k_person2id = p_personid
+                              AND p_personid = p1.m_creatorid 
+                              AND p1.m_c_replyof = p2.m_messageid
+                              AND p2.m_c_replyof is null
+                              AND p2.m_messageid = mt_messageid
+                              AND mt_tagid = t_tagid
+      GROUP by p_personid, p_firstname, p_lastname, t_name
+      ) x
+      GROUP by p_personid, p_firstname, p_lastname
+      ORDER by replyCount desc, p_personid;
