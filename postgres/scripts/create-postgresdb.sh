b82edc9234e6d43597871c83ae158783fa3f89af
David Puroja <dpuroja@outlook.com>
2022-05-24 22:39:22 +0200

    Fix postgres loading script

--- postgres/scripts/create-postgresdb.sh
+++ postgres/scripts/create-postgresdb.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 <<-EOSQL
-    DROP DATABASE IF EXISTS ${POSTGRES_DB};
-EOSQL
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER ${POSTGRES_USER};
-	CREATE DATABASE ${POSTGRES_DB};
+    DO
+    \$do\$
+    BEGIN
+    IF EXISTS (
+        SELECT FROM pg_catalog.pg_roles
+        WHERE  rolname = '${POSTGRES_USER}') THEN
+
+        RAISE NOTICE 'Role ${POSTGRES_USER} already exists. Skipping.';
+    ELSE
+        BEGIN   -- nested block
+            CREATE ROLE ${POSTGRES_USER} LOGIN PASSWORD '${POSTGRES_PASSWORD}';
+        EXCEPTION
+            WHEN duplicate_object THEN
+                RAISE NOTICE 'Role ${POSTGRES_USER} was just created by a concurrent transaction. Skipping.';
+        END;
+    END IF;
 	GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
+    END
+    \$do\$;
 EOSQL
