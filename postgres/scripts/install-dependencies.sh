b10bf4f4cdb733354de5b84a316c054b78b2bc62
David Puroja <dpuroja@outlook.com>
2022-05-27 12:17:52 +0200

    Move dependency to postgres install script

--- postgres/scripts/install-dependencies.sh
+++ postgres/scripts/install-dependencies.sh
@@ -4,5 +4,5 @@ set -eu
 set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+sudo apt update && sudo apt install -y libpq5
 pip3 install --user --progress-bar off psycopg
