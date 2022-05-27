c42b860824a17f10d702152ecad64b46c6dc7ad6
David Puroja <dpuroja@outlook.com>
2022-05-27 13:26:57 +0200

    Remove database option in test-db-connection

--- postgres/scripts/test-db-connection.py
+++ postgres/scripts/test-db-connection.py
@@ -4,7 +4,6 @@ import os
 import psycopg
 
 conn = psycopg.connect(
-        dbname=os.environ.get("POSTGRES_DB", "ldbcsnb"),
         host=os.environ.get("POSTGRES_HOST", "localhost"),
         user=os.environ.get("POSTGRES_USER", "postgres"),
         password=os.environ.get("POSTGRES_PASSWORD", "mysecretpassword"),
