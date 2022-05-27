bd67aa5b323e489b7171a75ab4afac48d8b6540a
David Puroja <dpuroja@outlook.com>
2022-05-27 11:43:11 +0200

    Move dependency

--- postgres/scripts/install-dependencies.sh
+++ postgres/scripts/install-dependencies.sh
@@ -6,5 +6,3 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pip3 install --user --progress-bar off psycopg
-export DEBIAN_FRONTEND=noninteractive
-apt -y install libpq5
