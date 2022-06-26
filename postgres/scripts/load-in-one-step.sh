2ad363908a580ed9433f581c9068ab2d7d00305f
Gabor Szarnyas <szarnyasg@gmail.com>
2022-06-26 16:54:18 +0200

    Cypher, Postgres: Fail early in load-in-one-step scripts

--- postgres/scripts/load-in-one-step.sh
+++ postgres/scripts/load-in-one-step.sh
@@ -6,6 +6,11 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
+if [ ! -d "${POSTGRES_CSV_DIR}" ]; then
+    echo "PostgreSQL directory does not exist. \${POSTGRES_CSV_DIR} is set to: ${POSTGRES_CSV_DIR}"
+    exit 1
+fi
+
 scripts/stop.sh
 scripts/start.sh
 scripts/create-db.sh
