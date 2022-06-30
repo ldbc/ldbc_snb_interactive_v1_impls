f6c6cf8fb99f466e0595f6ce1515136ba9ca3979
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-01 00:27:49 +0200

    Cypher: Update sample data set scripts, use it in CI

--- cypher/scripts/get-sample-data-set.sh
+++ cypher/scripts/get-sample-data-set.sh
@@ -6,7 +6,7 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-rm -f social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed.zip
-wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed.zip
-rm -rf social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed/
-unzip -q social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed.zip
+rm -f social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed-epoch-millis.zip
+wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed-epoch-millis.zip
+rm -rf social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed-epoch-millis/
+unzip -q social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed-epoch-millis.zip
--- cypher/scripts/use-sample-data-set.sh
+++ cypher/scripts/use-sample-data-set.sh
@@ -1,4 +1,4 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-export NEO4J_CSV_DIR=`pwd`/social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed/graphs/csv/bi/composite-projected-fk/
+export NEO4J_CSV_DIR=`pwd`/social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed-epoch-millis/graphs/csv/bi/composite-projected-fk/
