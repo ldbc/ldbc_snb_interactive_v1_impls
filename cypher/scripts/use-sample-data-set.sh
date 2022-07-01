f73f0d8101e19de24429e71aba1ed8178b0f6d4f
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-01 21:31:31 +0200

    Further simplify use-sample-data-set.sh scripts

--- cypher/scripts/use-sample-data-set.sh
+++ cypher/scripts/use-sample-data-set.sh
@@ -1,4 +1,2 @@
-cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
-
 export SF=0.003
 export NEO4J_CSV_DIR=`pwd`/../social-network-sf${SF}-bi-composite-projected-fk-neo4j-compressed-epoch-millis/graphs/csv/bi/composite-projected-fk/
--- postgres/scripts/use-sample-data-set.sh
+++ postgres/scripts/use-sample-data-set.sh
@@ -1,4 +1,2 @@
-cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
-
 export SF=0.003
 export POSTGRES_CSV_DIR=`pwd`/../social-network-sf${SF}-bi-composite-merged-fk/graphs/csv/bi/composite-merged-fk/
