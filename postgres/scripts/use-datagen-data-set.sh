f4af3ef7e013930a0305ac30c0c77bc375e35839
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-01 21:28:00 +0200

    Simplify use-*-data-set.sh scripts

--- postgres/scripts/use-datagen-data-set.sh
+++ postgres/scripts/use-datagen-data-set.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-pushd . > /dev/null
-
-cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
-cd ..
-
 export POSTGRES_CSV_DIR=${LDBC_SNB_DATAGEN_DIR}/out-sf${SF}/graphs/csv/bi/composite-merged-fk/
-
-popd > /dev/null
