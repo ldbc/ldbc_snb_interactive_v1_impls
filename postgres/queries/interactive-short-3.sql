ef81c2bf9da28938879f892f788050a7488894e8
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-06 15:13:01 +0200

    Postgres: Fix sorting attribute for IS3

--- postgres/queries/interactive-short-3.sql
+++ postgres/queries/interactive-short-3.sql
@@ -5,5 +5,5 @@ SELECT Person.id, Person.firstName, person.lastName, person.creationDate
 FROM Person_knows_Person k, Person
 WHERE k.Person1Id = :personId
   AND k.Person2Id = Person.id
-ORDER BY Person.creationDate DESC, Person.id ASC
+ORDER BY k.creationDate DESC, Person.id ASC
 ;
