b1850f20837b7fd7da2ed1b2c1c82fef80600076
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-06 16:02:09 +0200

    Postgres: Fix DEL 8 query

--- postgres/queries/interactive-delete-8.sql
+++ postgres/queries/interactive-delete-8.sql
@@ -3,6 +3,6 @@
 \set person2Id 4398046511230
  */
 DELETE FROM Person_knows_Person
-WHERE Person1Id = :person1Id
-  AND Person2Id = :person2Id
+WHERE (Person1Id = :person1Id AND Person2Id = :person2Id)
+   OR (Person1Id = :person2Id AND Person2Id = :person1Id)
 ;
