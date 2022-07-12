3c13ce6c07f2e39a4d77d1ac84f7cb1b2507ca4e
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-12 09:34:25 +0200

    Postgres: Remove PK constraints from edges

--- postgres/ddl/schema-primary-keys.sql
+++ postgres/ddl/schema-primary-keys.sql
@@ -2,11 +2,3 @@
 ALTER TABLE Message ADD PRIMARY KEY (id);
 ALTER TABLE Forum ADD PRIMARY KEY (id);
 ALTER TABLE Person ADD PRIMARY KEY (id);
-ALTER TABLE Forum_hasMember_Person ADD PRIMARY KEY (ForumId, PersonId);
-ALTER TABLE Forum_hasTag_Tag ADD PRIMARY KEY (ForumId, TagId);
-ALTER TABLE Person_hasInterest_Tag ADD PRIMARY KEY (PersonId, TagId);
-ALTER TABLE Person_knows_Person ADD PRIMARY KEY (Person1Id, Person2Id);
-ALTER TABLE Person_likes_Message ADD PRIMARY KEY (PersonId, id);
-ALTER TABLE Person_studyAt_University ADD PRIMARY KEY (PersonId, UniversityId);
-ALTER TABLE Person_workAt_Company ADD PRIMARY KEY (PersonId, CompanyId);
-ALTER TABLE Message_hasTag_Tag ADD PRIMARY KEY (id, TagId);
