c5c0e8738b0133f35dde3ae95da7cf546bc7b1c5
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-21 18:39:29 +0200

    Postgres: Fix return value

--- postgres/src/main/java/org/ldbcouncil/snb/impls/workloads/postgres/PostgresDb.java
+++ postgres/src/main/java/org/ldbcouncil/snb/impls/workloads/postgres/PostgresDb.java
@@ -293,7 +293,7 @@ public abstract class PostgresDb extends BaseDb<PostgresQueryStore> {
         @Override
         public LdbcQuery14Result convertSingleResult(ResultSet result) throws SQLException {
             return new LdbcQuery14Result(
-                    PostgresConverter.convertLists(result),
+                    PostgresConverter.convertLists(PostgresConverter.arrayToObjectArray(result, 1)),
                     result.getLong(2));
         }
 
