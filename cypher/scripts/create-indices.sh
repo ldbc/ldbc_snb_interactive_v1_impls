5320ad2179ecf30ed64e4fce2eb0403725a45542
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-27 20:49:23 +0200

    Cypher: Reconcile indices.cypher files

--- cypher/scripts/create-indices.sh
+++ cypher/scripts/create-indices.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 
-docker exec --interactive ${NEO4J_CONTAINER_NAME} cypher-shell < indices.cypher
+docker exec --interactive ${NEO4J_CONTAINER_NAME} cypher-shell < ../ddl/indices.cypher
