e4618cacf9bcb7843577c6ff1c864611257e5b30
Gabor Szarnyas <szarnyasg@gmail.com>
2022-08-01 23:52:23 +0200

    Postgres: Add more logging and more frequent commits to loading script

--- postgres/scripts/load.py
+++ postgres/scripts/load.py
@@ -2,6 +2,8 @@
 
 import os
 import psycopg
+import re
+import time
 
 class PostgresDbLoader():
 
@@ -11,6 +13,32 @@ class PostgresDbLoader():
         self.port = int(os.environ.get("POSTGRES_PORT", 5432))
         self.user = os.environ.get("POSTGRES_USER", "postgres")
         self.password = os.environ.get("POSTGRES_PASSWORD", "mysecretpassword")
+
+    def run_script(self, conn, filename):
+
+        with open(filename, "r") as f:
+            try:
+                queries_file = f.read()
+                # strip comments
+                queries_file = re.sub(r"(\n|^)--.*", "", queries_file)
+                queries = queries_file.split(";")
+                for query in queries:
+                    if query.isspace():
+                        continue
+
+                    sql_statement = re.findall(r"^((CREATE|INSERT|DROP|DELETE|SELECT|COPY) [A-Za-z0-9_ ]*)", query, re.MULTILINE|re.IGNORECASE)
+                    print(f"{sql_statement[0][0].strip()} ...")
+                    start = time.time()
+                    cur = conn.cursor()
+                    cur.execute(query)
+                    conn.commit()
+                    end = time.time()
+                    duration = end - start
+                    print(f"-> {duration:.4f} seconds")
+            except Exception:
+                print(f"Error trying to execute query: {query};")
+                raise
+
 
     def vacuum(self, conn):
         conn.autocommit=True
@@ -25,19 +53,16 @@ class PostgresDbLoader():
             password=self.password,
             port=self.port
         ) as conn:
-            with conn.cursor() as cur:
-                print("Loading initial data set")
-                cur.execute(self.load_script("ddl/schema.sql"))
-                cur.execute(self.load_script("ddl/load.sql"))
-                conn.commit()
+            print("Loading initial data set")
+            self.run_script(conn, "ddl/schema.sql")
+            self.run_script(conn, "ddl/load.sql")
 
-                print("Adding indexes and constraints")
-                cur.execute(self.load_script("ddl/schema_constraints.sql"))
-                cur.execute(self.load_script("ddl/schema_foreign_keys.sql"))
-                conn.commit()
+            print("Adding indexes and constraints")
+            self.run_script(conn, "ddl/schema_constraints.sql")
+            self.run_script(conn, "ddl/schema_foreign_keys.sql")
 
-                print("Vacuuming")
-                self.vacuum(conn)
+            print("Vacuuming")
+            self.vacuum(conn)
             conn.commit()
         print("Loaded initial snapshot")
 
