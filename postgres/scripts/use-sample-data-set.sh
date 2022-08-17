f98c7282a3834e1ee41bab293951298b17733e55
Gabor Szarnyas <szarnyasg@gmail.com>
2022-08-17 15:14:41 +0200

    Postgres: Allow loading from compressed (.csv.gz) files

--- postgres/scripts/use-sample-data-set.sh
+++ postgres/scripts/use-sample-data-set.sh
@@ -1,2 +1,2 @@
 export SF=0.003
-export POSTGRES_CSV_DIR=`pwd`/social-network-sf${SF}-bi-composite-merged-fk/graphs/csv/bi/composite-merged-fk/
+export POSTGRES_CSV_DIR=`pwd`/social-network-sf${SF}-bi-composite-merged-fk-postgres-compressed/graphs/csv/bi/composite-merged-fk/
