7547f925cb0ea5b487171719ecfc9232e37165da
Gabor Szarnyas <szarnyasg@gmail.com>
2022-08-17 12:02:45 +0200

    Paramgen: Add Q13 and Q14 implementations
    Variants (a) are based on the 'personKnowsPersonConnected' connected components factor table
    
    Variants (b) are based on the person4Hops factor table

--- paramgen/paramgen-queries/pg-13a.sql
+++ paramgen/paramgen-queries/pg-13a.sql
@@ -1,22 +1,20 @@
 -- variant (a): guaranteed that no path exists
 SELECT
-    person1Id AS 'person1Id',
-    person2Id AS 'person2Id'
+    component1.PersonId AS 'person1Id',
+    component2.PersonId AS 'person2Id'
 FROM
-    (SELECT
-        id AS person1Id,
-        abs(frequency - (SELECT percentile_disc(0.43) WITHIN GROUP (ORDER BY frequency) FROM personNumFriends)) AS diff
-    FROM personNumFriends
-    ORDER BY diff, md5(id)
-    LIMIT 30
-    ),
-    (SELECT
-        id AS person2Id,
-        abs(frequency - (SELECT percentile_disc(0.37) WITHIN GROUP (ORDER BY frequency) FROM personNumFriends)) AS diff
-    FROM personNumFriends
-    ORDER BY diff, md5(id)
-    LIMIT 30
-    )
-WHERE person1Id != person2Id
-ORDER BY md5(concat(person1Id, person2Id))
+    (
+        SELECT PersonId, Component
+        FROM personKnowsPersonConnected
+        ORDER BY md5(PersonId + 1)
+        LIMIT 100
+    ) component1,
+    (
+        SELECT PersonId, Component
+        FROM personKnowsPersonConnected
+        ORDER BY md5(PersonId + 2)
+        LIMIT 100
+    ) component2
+WHERE component1.Component != component2.Component
+ORDER BY md5(concat(component1.PersonId, component2.PersonId))
 LIMIT 500
--- paramgen/paramgen-queries/pg-14b.sql
+++ paramgen/paramgen-queries/pg-14b.sql
@@ -2,21 +2,6 @@
 SELECT
     person1Id AS 'person1Id',
     person2Id AS 'person2Id'
-FROM
-    (SELECT
-        id AS person1Id,
-        abs(frequency - (SELECT percentile_disc(0.73) WITHIN GROUP (ORDER BY frequency) FROM personNumFriends)) AS diff
-    FROM personNumFriends
-    ORDER BY diff, md5(id)
-    LIMIT 30
-    ),
-    (SELECT
-        id AS person2Id,
-        abs(frequency - (SELECT percentile_disc(0.37) WITHIN GROUP (ORDER BY frequency) FROM personNumFriends)) AS diff
-    FROM personNumFriends
-    ORDER BY diff, md5(id)
-    LIMIT 30
-    )
-WHERE person1Id != person2Id
-ORDER BY md5(concat(person1Id, person2Id))
+FROM people4Hops
+ORDER BY md5(concat(person1Id + 3, person2Id + 4))
 LIMIT 500
