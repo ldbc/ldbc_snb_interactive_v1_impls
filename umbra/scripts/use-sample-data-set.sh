6c8640819a3b4a0f0bc199b4a31eb909a914314c
Gabor Szarnyas <szarnyasg@gmail.com>
2022-08-18 10:36:28 +0200

    Umbra: Add scripts for fetching (uncompressed) data sets

--- umbra/scripts/use-sample-data-set.sh
+++ umbra/scripts/use-sample-data-set.sh
@@ -1,2 +1,2 @@
 export SF=0.003
-export UMBRA_CSV_DIR=`pwd`/../postgres/social-network-sf${SF}-bi-composite-merged-fk/graphs/csv/bi/composite-merged-fk/
+export UMBRA_CSV_DIR=`pwd`/social-network-sf${SF}-bi-composite-merged-fk/graphs/csv/bi/composite-merged-fk/
