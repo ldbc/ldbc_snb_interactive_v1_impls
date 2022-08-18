dffeb2fc1605d6f593089c5e46c1247eb2e368fb
David Puroja <dpuroja@outlook.com>
2022-08-18 22:05:47 +0200

    Fix tag update queries

--- mssql/queries/interactive-update-6-add-post-tags.sql
+++ mssql/queries/interactive-update-6-add-post-tags.sql
@@ -1,9 +1,12 @@
 INSERT INTO Message_hasTag_Tag (
+    $from_id, $to_id, 
     creationDate
   , MessageId
   , TagId
 )
 SELECT
+    (SELECT $NODE_ID FROM Message WHERE MessageId = :postId),
+    (SELECT $NODE_ID FROM Tag WHERE id = :tagId),
     :creationDate
   , :postId
   , :tagId
--- mssql/queries/interactive-update-7-add-comment-tags.sql
+++ mssql/queries/interactive-update-7-add-comment-tags.sql
@@ -1,9 +1,12 @@
 INSERT INTO Message_hasTag_Tag (
+    $from_id, $to_id, 
     creationDate
   , MessageId
   , TagId
 )
 SELECT
+    (SELECT $NODE_ID FROM Message WHERE MessageId = :commentId),
+    (SELECT $NODE_ID FROM Tag WHERE id = :tagId),
     :creationDate
   , :commentId
   , :tagId
