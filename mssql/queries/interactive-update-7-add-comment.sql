4c654f9025c8094b041fbad131c415558bad9491
David Puroja <dpuroja@outlook.com>
2022-08-19 10:05:41 +0200

    Maintain weight

--- mssql/queries/interactive-update-7-add-comment.sql
+++ mssql/queries/interactive-update-7-add-comment.sql
@@ -1,3 +1,6 @@
+DECLARE @replyToMessageId bigint;
+SET @replyToMessageId = :replyToCommentId + :replyToPostId
+
 INSERT INTO Message (
     creationDate
   , MessageId
@@ -25,5 +28,13 @@ VALUES
   , :authorPersonId -- CreatorPersonId
   , NULL
   , :countryId -- LocationCountryId
-  , :replyToCommentId + :replyToPostId
+  , @replyToMessageId
 );
+
+DECLARE @ParentPersonId bigint;
+SET @ParentPersonId = (SELECT CreatorPersonId FROM Message WHERE MessageId = @replyToMessageId);
+
+UPDATE Person_knows_Person
+SET weight = dbo.CalculateInteractionScore(:authorPersonId, @ParentPersonId)
+WHERE (person1Id = :authorPersonId AND person2Id = @ParentPersonId )
+   OR (person2Id = :authorPersonId AND person1Id = @ParentPersonId);
