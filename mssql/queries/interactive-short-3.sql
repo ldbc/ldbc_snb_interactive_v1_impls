e1c5368fc5b9410e4a12aae223700d560e1ced7e
David Puroja <dpuroja@outlook.com>
2022-08-23 16:08:50 +0200

    Fix SQ3

--- mssql/queries/interactive-short-3.sql
+++ mssql/queries/interactive-short-3.sql
@@ -5,5 +5,5 @@ SELECT Person.personId, Person.firstName, person.lastName, person.creationDate
 FROM Person_knows_Person k, Person
 WHERE k.Person1Id = :personId
   AND k.Person2Id = Person.personId
-ORDER BY Person.creationDate DESC, Person.personId ASC
+ORDER BY k.creationDate DESC, Person.personId ASC
 ;
