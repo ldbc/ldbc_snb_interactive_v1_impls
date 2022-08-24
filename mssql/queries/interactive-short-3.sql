b31e796a2cfc4ba4b2f4d38e6bd05acccc4cd312
David Puroja <dpuroja@outlook.com>
2022-08-24 12:13:30 +0200

    CHange IC2, fix IS3

--- mssql/queries/interactive-short-3.sql
+++ mssql/queries/interactive-short-3.sql
@@ -1,7 +1,7 @@
 /* IS3. Friends of a Person
 \set personId 17592186044461
  */
-SELECT Person.personId, Person.firstName, person.lastName, person.creationDate
+SELECT Person.personId, Person.firstName, person.lastName, k.creationDate
 FROM Person_knows_Person k, Person
 WHERE k.Person1Id = :personId
   AND k.Person2Id = Person.personId
