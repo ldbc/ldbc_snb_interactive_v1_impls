8b066c016bff7b9c4fd8401134619af5ba0b1233
David Puroja <dpuroja@outlook.com>
2022-08-24 12:13:57 +0200

    Q12 WIP

--- mssql/queries/interactive-complex-12.sql
+++ mssql/queries/interactive-complex-12.sql
@@ -1,44 +1,48 @@
 WITH extended_tags(s_subtagclassid,s_supertagclassid) AS (
-    SELECT id
-         , id
+    SELECT id, id
       FROM tagclass
      UNION ALL
-    SELECT tc.id
-         , t.s_supertagclassid
-      FROM tagclass tc
-         , extended_tags t
+    SELECT tc.id, t.s_supertagclassid
+      FROM tagclass tc, extended_tags t
      WHERE tc.SubclassOfTagClassId = t.s_subtagclassid
 )
-SELECT TOP(20) personId
-             , firstName
-             , lastName
-             , string_agg(CONVERT(NVARCHAR(max),name), ';')
-             , sum(partialReplyCount)
-            AS replyCount
-          FROM ( SELECT DISTINCT Person.personId, firstName, lastName, name, count(*) AS partialReplyCount
-                   FROM Person
-                      , Message m1
-                      , Person_knows_Person k
-                      , Message m2
-                      , Message_hasTag_Tag
-                      , (SELECT DISTINCT id
-                              , name 
-                           FROM tag
-                          WHERE (TypeTagClassId IN (
-                                 SELECT DISTINCT s_subtagclassid
-                                   FROM extended_tags k
-                                      , tagclass
-                                  WHERE id = k.s_supertagclassid
-                                    AND name = :tagClassName
-                                ))) selected_tags
-                        WHERE Person1Id = :personId
-                        AND k.Person2Id = Person.personId
-                        AND Person.personId = m1.CreatorPersonId
-                        AND m1.ParentMessageId = m2.MessageId
-                        AND m2.ParentMessageId IS NULL
-                        AND m2.MessageId = Message_hasTag_Tag.MessageId
-                        AND Message_hasTag_Tag.TagId = selected_tags.id
-      GROUP BY Person.personId, Person.firstName, person.lastName, name
-      ) x
-      GROUP BY personId, firstName, lastName
-      ORDER BY replyCount DESC, personId ASC;
+SELECT DISTINCT id, name 
+INTO #SelectedTags
+FROM tag t 
+WHERE EXISTS (
+	SELECT s_subtagclassid
+	FROM extended_tags k, tagclass
+	WHERE id = k.s_supertagclassid
+	AND name = :tagClassName
+	AND t.TypeTagClassId = s_subtagclassid
+)
+
+SELECT TOP(20)
+        Person2.personId as friendPersonId,
+        Person2.firstName as friendFirstName,
+        Person2.lastName as friendLastName,
+        string_agg(CONVERT(NVARCHAR(max), st.name), ';'),
+        count(DISTINCT Message1.MessageId) as replyCount
+FROM 	Person Person1,
+        Person_knows_Person,
+        Person Person2,
+        Message_hasCreator_Person,
+        Message_replyOf_Message,
+        Message_hasTag_Tag,
+        Tag Tag1,
+        Message Message1,
+        Message Message2,
+    	#SelectedTags as st
+WHERE MATCH (
+        Person1-(Person_knows_Person)->Person2
+        AND Message1-(Message_hasCreator_Person)->Person2
+        AND Message1-(Message_replyOf_Message)->Message2
+        AND Message2-(Message_hasTag_Tag)->Tag1
+)
+AND Person1.personId = :personId
+AND Person2.personId = Message1.CreatorPersonId
+AND Message2.ParentMessageId IS NULL
+AND Tag1.id = st.id
+GROUP BY Person2.personId, Person2.firstName, Person2.lastName
+ORDER BY replyCount DESC, Person2.personId ASC;
+DROP TABLE #SelectedTags
