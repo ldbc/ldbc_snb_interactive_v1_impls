be58543fe322940407267c5a192514cdd94f71a9
David Puroja <dpuroja@outlook.com>
2022-08-25 17:22:08 +0200

    Add edge upon message creation

--- mssql/queries/interactive-update-7-add-comment.sql
+++ mssql/queries/interactive-update-7-add-comment.sql
@@ -27,3 +27,10 @@ VALUES
   , :countryId -- LocationCountryId
   , :replyToCommentId + :replyToPostId
 );
+
+INSERT INTO Message_replyOf_Message($from_id, $to_id)
+VALUES
+(
+    (SELECT $NODE_ID FROM Message WHERE MessageId = :commentId),
+    (SELECT $NODE_ID FROM Message WHERE MessageId = :replyToCommentId + :replyToPostId)
+);
