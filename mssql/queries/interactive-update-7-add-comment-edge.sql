c1840558f0a0e3f5993b8aeef466aeb1ad1ee8d5
David Puroja <dpuroja@outlook.com>
2022-08-28 18:05:11 +0200

    Remove weight update queries

--- mssql/queries/interactive-update-7-add-comment-edge.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO Message_replyOf_Message($from_id, $to_id)
-VALUES
-(
-    (SELECT $NODE_ID FROM Message WHERE MessageId = :commentId),
-    (SELECT $NODE_ID FROM Message WHERE MessageId = :replyToCommentId + :replyToPostId)
-);
