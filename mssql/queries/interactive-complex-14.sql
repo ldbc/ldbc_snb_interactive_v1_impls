8c86bbafa786acb90e373fef9be1e68f17f348c5
David Puroja <dpuroja@outlook.com>
2022-08-29 14:49:28 +0200

    Cleanup Q14

--- mssql/queries/interactive-complex-14.sql
+++ mssql/queries/interactive-complex-14.sql
@@ -7,69 +7,11 @@ DECLARE @trustedPaths as table
 (
     Id bigint,
     OrderDiscovered int,
-    Path varchar(MAX)
-)
-
-DECLARE @pathWithScore as table
-(
     Path varchar(MAX),
-    score bigint
-)
-
-DECLARE @interactions as table
-(
-    person1Id bigint,
-    person2Id bigint,
     score bigint
 )
 
 INSERT INTO @trustedPaths
 EXEC dbo.knows_Breadth_First :person1Id, :person2Id;
 
-
-DECLARE @Path VARCHAR(MAX);
-DECLARE @Score bigint;
-DECLARE @person1Id bigint;
-DECLARE @person2Id bigint;
-
-WHILE (Select Count(*) FROM @trustedPaths) > 0
-BEGIN
-
-    SELECT TOP 1 @Path = Path FROM @trustedPaths
-    DROP TABLE IF EXISTS #Temp
-
-    SELECT items AS id, rownum
-    INTO #Temp
-    FROM dbo.Split(@Path, ';')
-
-    SELECT TOP 1 @person1Id = id FROM #Temp
-    DELETE #Temp WHERE id = @person1Id
-    WHILE (SELECT Count(*) FROM #Temp) > 0
-    BEGIN
-        SELECT TOP 1 @person2Id = id FROM #Temp
-
-        SET @Score = (SELECT weight from Person_knows_Person WHERE Person1Id = @Person1Id AND Person2Id = @Person2Id)
-        IF @Score = 0
-            BEGIN
-                DELETE FROM @interactions
-                DROP TABLE IF EXISTS #Temp
-                BREAK
-            END
-        ELSE
-            BEGIN
-                INSERT INTO @interactions VALUES(@person1Id, @person2Id, FLOOR(40 - SQRT(@Score)))
-            END
-        SET @person1Id = @person2Id
-        DELETE #Temp WHERE id = @person2Id
-        SELECT Top 1 @person2Id = id FROM #Temp
-    END
-    DELETE @trustedPaths WHERE Path = @Path
-        IF @Score > 0
-            BEGIN
-                INSERT INTO @pathWithScore
-                SELECT @Path, SUM(score) FROM @interactions;
-            END
-    DELETE FROM @interactions;
-END
-
-SELECT TOP(1) * FROM @pathWithScore ORDER BY score;
+SELECT TOP(1) Path, score FROM @trustedPaths;
