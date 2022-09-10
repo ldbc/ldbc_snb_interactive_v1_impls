e3ebc31f7481fa24e274c2dfc3b4bfd29131b6f7
David Puroja <dpuroja@outlook.com>
2022-09-10 19:36:24 +0200

    Fix column reference

--- paramgen/paramgen-queries/pg-7.sql
+++ paramgen/paramgen-queries/pg-7.sql
@@ -5,7 +5,7 @@ FROM
         id AS personId,
         abs(frequency - (SELECT percentile_disc(0.45) WITHIN GROUP (ORDER BY frequency) FROM personNumFriends)) AS diff
     FROM personNumFriends
-    WHERE numFriends > 0 AND deletionDate > '2019' AND creationDate < '2012-11-29'
+    WHERE frequency > 0 AND deletionDate > '2019' AND creationDate < '2012-11-29'
     ORDER BY diff, md5(id)
     LIMIT 500
     )
