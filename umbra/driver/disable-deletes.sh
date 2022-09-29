49cf808a311de84e255b789e3181670b18bfff54
Gabor Szarnyas <szarnyasg@gmail.com>
2022-09-29 10:03:29 +0200

    Fix enable/disable scripts to allow whitespaces around '='

--- umbra/driver/disable-deletes.sh
+++ umbra/driver/disable-deletes.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sed -i 's/\(ldbc.snb.interactive.LdbcDelete.*\)_enable=true/\1_enable=false/' *.properties
+sed -i 's/\(ldbc.snb.interactive.LdbcDelete.*_enable\s*=\s*\)true/\1false/' *.properties
