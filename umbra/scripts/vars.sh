22a1de5e11d019c3999e776cc3d1a42cc408ed66
Gabor Szarnyas <szarnyasg@gmail.com>
2022-11-10 17:41:24 +0100

    Umbra: Split create DB command

--- umbra/scripts/vars.sh
+++ umbra/scripts/vars.sh
@@ -2,6 +2,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd 
 cd ..
 
 export UMBRA_DDL_DIR=`pwd`/ddl/
+export UMBRA_LOG_DIR=`pwd`/scratch/log/
 export UMBRA_DATABASE_DIR=`pwd`/scratch/db/
 export UMBRA_BACKUP_DIR=`pwd`/scratch/backup/
 export UMBRA_CONTAINER_NAME=snb-interactive-umbra
