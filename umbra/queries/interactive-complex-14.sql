ae09682feb8b5ba6c28161f2a95fd5ab7264e06a
Gabor Szarnyas <szarnyasg@gmail.com>
2022-11-10 17:31:18 +0000

    Umbra: Fix Q14 subquery, enable it in benchmark mode

--- umbra/queries/interactive-complex-14.sql
+++ umbra/queries/interactive-complex-14.sql
@@ -82,7 +82,7 @@ path0r(v, path, l, w) as (
     from path0
     where l0 = -1
 ),
-path1(v, path, l) as (
+path1(v, path, l, w) as (
     select v, path, l, w from path0r
     union all
     select ss.prev, array_append(path, ss.prev), l - 1, w + coalesce(score, 0)
