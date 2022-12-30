55d6e35211dd912b6bbf94379baa08af98532cd5
David Puroja <dpuroja@outlook.com>
2022-12-30 09:13:06 +0000

    Add PK to Person_likes_Message edge table

--- mssql/ddl/schema-constraints.sql
+++ mssql/ddl/schema-constraints.sql
@@ -4,6 +4,7 @@ ALTER TABLE Forum_hasMember_Person ADD PRIMARY KEY (ForumId, PersonId);
 ALTER TABLE Forum_hasTag_Tag ADD PRIMARY KEY (ForumId, TagId);
 ALTER TABLE Person_hasInterest_Tag ADD PRIMARY KEY (PersonId, TagId);
 ALTER TABLE Person_knows_Person ADD PRIMARY KEY (Person1Id, Person2Id);
+ALTER TABLE Person_likes_Message ADD PRIMARY KEY (PersonId, MessageId);
 ALTER TABLE Person_studyAt_University ADD PRIMARY KEY (PersonId, UniversityId);
 ALTER TABLE Person_workAt_Company ADD PRIMARY KEY (PersonId, CompanyId);
 ALTER TABLE Message_hasTag_Tag ADD PRIMARY KEY (MessageId, TagId);
