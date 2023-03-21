07b7706f1d7cf5e974afcb1cc1ae7946d132846b
Gabor Szarnyas <szarnyasg@gmail.com>
2023-03-21 11:27:09 +0100

    Use apt-get instead of apt in dependency installation scripts

--- umbra/scripts/install-dependencies.sh
+++ umbra/scripts/install-dependencies.sh
@@ -7,9 +7,9 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [[ ! -z $(which yum) ]]; then
     sudo yum install -y postgresql-devel
-elif [[ ! -z $(which apt) ]]; then
-    sudo apt update
-    sudo apt install -y libpq5
+elif [[ ! -z $(which apt-get) ]]; then
+    sudo apt-get update
+    sudo apt-get install -y libpq5
 else
     echo "Operating system not supported, please install the dependencies manually"
 fi
